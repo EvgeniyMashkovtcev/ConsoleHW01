be1ecf4fcbc38ab0ec1b47c4c62522bd18910bde
evgenii_m <teken25424biz@gmail.com>
2024-04-03 13:03:15 +0300

    add hw 02

--- Program.cs
+++ Program.cs
@@ -4,8 +4,73 @@ namespace ConsoleHW01
 {
     internal class Program
     {
+        class Bits
+        {
+            public Bits(byte b)
+            {
+                this.Value = b;
+            }
+            public byte Value { get; private set; } = 0;
+            public bool this[int index]
+            {
+                get
+                {
+                    if (index > 7 || index < 0)
+                        return false;
+                    return ((Value >> index) & 1) == 1;
+                }
+                set
+                {
+                    if (index > 7 || index < 0) return;
+                    if (value == true)
+                        Value = (byte)(Value | (1 << index));
+                    else
+                    {
+                        var mask = (byte)(1 << index);
+                        mask = (byte)(0xff ^ mask);
+                        Value &= (byte)(Value & mask);
+                    }
+                }
+
+            }
+
+            public static implicit operator byte(Bits b) => b.Value;
+
+
+            // Домашняя работа:
+            public static implicit operator Bits(long value) => new Bits((byte)value);
+
+            public static implicit operator Bits(int value) => new Bits((byte)value);
+
+            public static implicit operator Bits(byte value) => new Bits(value);
+        }
+
         static void Main(string[] args)
         {
+            Bits bitsFromLong = 12345L;
+            Bits bitsFromInt = 205;
+            Bits bitsFromByte = (byte)20;
+
+            Console.WriteLine("Bits from long: " + Convert.ToString(bitsFromLong.Value, 2));
+            Console.WriteLine("Bits from int: " + Convert.ToString(bitsFromInt.Value, 2));
+            Console.WriteLine("Bits from byte: " + Convert.ToString(bitsFromByte.Value, 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
             var grandMother = new FamilyMember() { Mother = null, Father = null, Name = "Вика", Sex = Gender.Female };
             var grandFather = new FamilyMember() { Mother = null, Father = null, Name = "Виктор", Sex = Gender.Male };
             var father = new FamilyMember() { Mother = null, Father = null, Name = "Илья", Sex = Gender.Male };
@@ -18,9 +83,9 @@ namespace ConsoleHW01
             father.AddChild(son);
             father.AddChild(daughter);
             mother.AddChild(daughter);
+            */
 
-
-            father.PrintFamilyTree();
+            // father.PrintFamilyTree();
             // mother.PrintFamilyTree();
             // son.PrintFamilyTree();
             // daughter.PrintFamilyTree();
